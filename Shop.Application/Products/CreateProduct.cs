0a6c8497446d5f38dd65ae3d5b6c4a421f0e42b3
Milan <m.topic96@live.com>
2020-03-05 11:30:50 +0100

    Add project files.

---
+++ Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,28 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Application.Products
+{
+    class CreateProducts
+    {
+        private ApplicationDbContext _context;
+
+        public CreateProducts(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string name, string description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Name = name,
+                Description = description
+            });
+        }
+    }
+}
---
+++ Shop.Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.Domain.Models;
+
+namespace Shop.Database
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
