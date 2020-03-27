962b3163a1833b618d296fcffd2c9629c6f29461
Milan <m.topic96@live.com>
2020-03-27 13:10:16 +0100

    added customer-info

--- Shop.Domain/Models/Order.cs
+++ Shop.Domain/Models/Order.cs
@@ -9,8 +9,14 @@ namespace Shop.Domain.Models
         public int Id { get; set; }
         public string OrderRef { get; set; }
 
-        public string Adress1 { get; set; }
-        public string Adress2 { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
         public string City { get; set; }
         public string PostCode { get; set; }      
         
